d6d508f9cd0e2aa927c89ed8cec8d3f217531bf4
mimi1595O <158983247+mimi1595O@users.noreply.github.com>
2025-09-03 06:47:07 +0700

    Add files via upload

---
+++ fukkyu.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+
+    int main(){
+
+        std::string fuckyou = "fuck you! ";
+        std::string fuckyoufile = fuckyou + ".txt";
+        for (int i = 0; i < 1; --i){
+            std::ofstream fuck_you(fuckyoufile);
+            fuck_you << fuckyou;
+            fuckyou = fuckyou + fuckyou;
+            fuckyoufile = fuckyou + ".txt";
+        }
+    return 0;
+    }
+
+
+
+
